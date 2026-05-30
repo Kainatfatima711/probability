1392f53f869929ee75d40f9be9819ae3f50a6a8c
Kainatfatima711 <kainatfetima@gmail.com>
2026-05-30 14:20:21 +0500

    Add files via upload

---
+++ Step Throat Part 1.py
@@ -0,0 +1,109 @@
+def find_prob(a,b):
+    if a == 1:
+        prob_a = 0.2
+        if b == 1:
+         prob_bga = 0.80
+        elif b == 2:
+           prob_bga == 0.15
+        else:
+           print("Invalid Choice")
+
+           prob_a_and_b = prob_a * prob_bga
+           print("Probability of b given a:" , prob_bga)
+           print("Probability og both the events occuring:" , prob_a_and_b)
+
+    elif a == 2:
+       prob_a = 0.8
+       if b == 1:
+          prob_bga = 0.02
+       elif b == 2:
+          prob_bga = 0.98
+       else:
+          print("Invalid Choice")
+
+       prob_a_and_b = prob_a * prob_bga
+       print("Probability of b given a:" , prob_bga)
+       print("Probability of both events occuring:" , prob_a_and_b)
+    else:
+       print("Invalid Choice")
+
+       print("Let's calculate probability. Please enter choices for the events...")
+
+       print("Person has Step Throat ?def find_prob(a,b)")
+    
+        
+    if b == 1:
+         prob_bga = 0.80
+    elif b == 2:
+           prob_bga == 0.15
+    else:
+           print("Invalid Choice")
+
+           prob_a_and_b = prob_a * prob_bga
+           print("Probability of b given a:" , prob_bga)
+           print("Probability og both the events occuring:" , prob_a_and_b)
+
+    elif a == 2:
+       prob_a = 0.8
+       if b == 1:
+          prob_bga = 0.02
+       elif b == 2:
+          prob_bga = 0.98
+       else:
+          print("Invalid Choice")
+
+       prob_a_and_b = prob_a * prob_bga
+       print("Probability of b given a:" , prob_bga)
+       print("Probability of both events occuring:" , prob_a_and_b)
+    else:
+       print("Invalid Choice")
+
+print("Let's calculate probability. Please enter choices for the events...")
+
+print("Person has Step Throat ?\n 1. Yes \n 2. No")
+a =(int(input("Enter your choice (1/2):")))
+
+print("Probabilities for event a and b:")
+find_prob(a,b)")
+a =(int(input("Enter your choice (1/2):")))
+
+print("Person has tested positive?def find_prob(a,b)")
+    if a == 1:
+        prob_a = 0.2
+        if b == 1:
+         prob_bga = 0.80
+        elif b == 2:
+           prob_bga == 0.15
+        else:
+           print("Invalid Choice")
+
+           prob_a_and_b = prob_a * prob_bga
+           print("Probability of b given a:" , prob_bga)
+           print("Probability og both the events occuring:" , prob_a_and_b)
+
+    elif a == 2:
+       prob_a = 0.8
+       if b == 1:
+          prob_bga = 0.02
+       elif b == 2:
+          prob_bga = 0.98
+       else:
+          print("Invalid Choice")
+
+       prob_a_and_b = prob_a * prob_bga
+       print("Probability of b given a:" , prob_bga)
+       print("Probability of both events occuring:" , prob_a_and_b)
+    else:
+       print("Invalid Choice")
+
+print("Let's calculate probability. Please enter choices for the events...")
+
+print("Person has Step Throat ?\n 1. Yes \n 2. No")
+a =int(input("Enter your choice (1/2):")))
+
+print("Person has tested positive?\n 1. Yes \n 2.No")
+b = int(input("Enter your choices (1/2):"))
+
+print("Probabilities for event a and b:")
+find_prob(a,b)
+
